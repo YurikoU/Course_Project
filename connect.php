53d2ec47c0e3c4d98d64f5d2bf7d2598562a9476
YurikoU <76763897+YurikoU@users.noreply.github.com>
2021-02-27 14:59:51 -0800

    Add files via upload

--- connect.php
+++ connect.php
@@ -1,6 +1,8 @@
 <?php
   try
   {
+
+/*
     //Connect to the localhost
     $dsn = 'mysql:host=localhost;
             port=3308;
@@ -8,15 +10,13 @@
             charset=utf8;'; 
     $username = 'root';
     $password = '';
+*/
 
-
-    /*
     //Connect to the Lamp Server
     $dsn = 'mysql:host=172.31.22.43;
             dbname=Yuriko200448500'; 
     $username = 'Yuriko200448500'; 
     $password = 'foJpzK_3NG';
-    */
 
 
     $dbo = new PDO($dsn, $username, $password);    
