1848d5d090358ab06ae35c92440428e5abd40cda
YurikoU <76763897+YurikoU@users.noreply.github.com>
2021-04-12 16:25:58 -0700

    Add files via upload

--- membership_top.php
+++ membership_top.php
@@ -5,45 +5,25 @@
   if (!isset($_SESSION["user"])) {
     $_SESSION["errors"][] = "Please login to access this content.";
     header("Location: login.php");
-    // End PHP operation
-    // Otherwise, application will load in the background, while they're not logged in 
     exit();
   }
 
   // Assign the user (for logged in users)
   $user = $_SESSION["user"];
-?>
+  include('Head_and_footer/head.php'); 
+  ?>
 
-
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="styles.css">
-
-    <title>Profile</title>
+  <title> MEMBERSITE | The Glory Hotel & Spa </title>
   </head>
 
   <body>
-    <?php include_once('login_process.php') ?>
-
     <div class="container">
       <header class="jumbotron my-5">
         <div class="row">
-          <div class="col-5">
-            <script src="https://www.avatarapi.com/js.aspx?email=<?= $user['email'] ?>&size=128"></script>
-				
-          </div>
-
-          <div class="col-7">
+          <div class="col-6">
             <h1 class="display-4">Hello <strong><?= "{$user['first_name']} {$user['last_name']}" ?></strong></h1>
-            <p class="lead">Welcome CHAMPION!</p>
             <hr class="my-4">
-            <p>
-              Your personalized experience awaits <strong>YOU!</strong>
-            </p>
+            <p>Your member ID: <strong><?= "{$user['member_id']}" ?></strong></p>
           </div>
         </div>
       </header>
