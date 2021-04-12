21dc0467925e62ba19e96fa38cf4d31c0a327b7a
YurikoU <76763897+YurikoU@users.noreply.github.com>
2021-04-12 16:48:28 -0700

    Add files via upload

--- membership_top.php
+++ membership_top.php
@@ -11,7 +11,7 @@
   // Assign the user (for logged in users)
   $user = $_SESSION["user"];
   include('Head_and_footer/head.php'); 
-  ?>
+?>
 
   <title> MEMBERSITE | The Glory Hotel & Spa </title>
   </head>
@@ -20,15 +20,25 @@
     <div class="container">
       <header class="jumbotron my-5">
         <div class="row">
-          <div class="col-6">
-            <h1 class="display-4">Hello <strong><?= "{$user['first_name']} {$user['last_name']}" ?></strong></h1>
-            <hr class="my-4">
-            <p>Your member ID: <strong><?= "{$user['member_id']}" ?></strong></p>
+          <div class="col-8">
+            <h1 class="display-4">Welcome <strong><?= "{$user['first_name']} {$user['last_name']}" ?></strong></h1>
           </div>
         </div>
       </header>
+      <main>
+        <h3>Your Personal Profile</h3>
+        <table class='table table-striped'><tbody>
+        <?PHP
+          echo "<tr><td>Member ID: " . $user['member_id'] . "</td></tr>
+                <tr><td>First Name: " . $user['first_name'] . "</td></tr>
+                <tr><td>Last Name: " . $user['last_name'] . "</td></tr>
+                <tr><td>Phone: " . $user['phone'] . "</td></tr>
+                <tr><td>E-Mail Address: " . $user['email'] . "</td></tr>";
+        ?>
+        </tbody></table>
+      <a class="btn btn-primary" href="logout.php">Logout</a>
+      </main>
 
-      <a class="btn" href="logout.php">Logout</a>
     </div>
   </body>
 </html>
